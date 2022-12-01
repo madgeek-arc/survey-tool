a74e20a470e0fd8d1b7c61414540706f1b9eade7
jbalasis <cs2180008@di.uoa.gr>
2022-12-01 14:21:43 +0200

    moved service guards to outer project

--- app/observatoryUi.module.ts
+++ app/observatoryUi.module.ts
@@ -1,20 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {HomeComponent} from "../../app/pages/home/home.component";
 import {CatalogueUiModule} from "../catalogue-ui/catalogue-ui.module";
 import {SurveyFormComponent} from "./pages/contributions-dashboard/my-surveys/survey-form/survey-form.component";
-import {DynamicFormModule} from "../catalogue-ui/pages/dynamic-form/dynamic-form.module";
 import {ContributionsDashboardComponent} from "./pages/contributions-dashboard/contributions-dashboard.component";
 import {ReusableComponentsModule} from "./shared/reusablecomponents/reusable-components.module";
 import {AuthenticationService} from "./services/authentication.service";
 import {AuthenticationGuardService} from "./services/authentication-guard.service";
 import {SurveyService} from "./services/survey.service";
-import {HttpInterceptorService} from "./services/http-interceptor.service";
 import {AcceptInvitationComponent} from "./pages/accept-invitation.component.ts/accept-invitation.component";
-import {NationalContributionsToEOSCGuardService} from "./services/nationalContributionsToEOSC-guard.service";
-import {ArchiveGuardService} from "./services/archiveGuard.service";
 import {ObservatoryUiRoutingModule} from "./observatoryUi-routing.module";
 import {SharedModule} from "../../app/shared/shared.module";
 
@@ -28,7 +23,6 @@ import {SharedModule} from "../../app/shared/shared.module";
   imports: [
     CommonModule,
     CatalogueUiModule,
-    DynamicFormModule,
     ReusableComponentsModule,
     FormsModule,
     ObservatoryUiRoutingModule,
@@ -37,13 +31,6 @@ import {SharedModule} from "../../app/shared/shared.module";
   providers: [
     AuthenticationService,
     AuthenticationGuardService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpInterceptorService,
-      multi: true
-    },
-    NationalContributionsToEOSCGuardService,
-    ArchiveGuardService,
     SurveyService
   ],
   exports: [
