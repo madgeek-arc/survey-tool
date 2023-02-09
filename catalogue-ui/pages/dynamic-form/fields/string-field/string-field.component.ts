5ea4d3487bd9231729e535b69b6c1b4edeffc0aa
jbalasis <cs2180008@di.uoa.gr>
2023-02-09 19:17:14 +0200

    git subrepo clone --branch=v4.0 git@github.com:madgeek-arc/catalogue-ui.git src/survey-tool/catalogue-ui/
    subrepo:
      subdir:   "src/survey-tool/catalogue-ui"
      merged:   "111666b"
    upstream:
      origin:   "git@github.com:madgeek-arc/catalogue-ui.git"
      branch:   "v4.0"
      commit:   "111666b"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

---
+++ catalogue-ui/pages/dynamic-form/fields/string-field/string-field.component.ts
@@ -0,0 +1,111 @@
+import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {Field, HandleBitSet} from "../../../../domain/dynamic-form-model";
+import {FormArray, FormControl, FormGroup, FormGroupDirective} from "@angular/forms";
+import {FormControlService} from "../../../../services/form-control.service";
+
+@Component({
+  selector: 'app-string-url-email-field',
+  templateUrl: './string-field.component.html',
+  styles: ['.clear-style { height: 0 !important;}']
+})
+
+export class StringFieldComponent implements OnInit {
+  @Input() fieldData: Field;
+  @Input() editMode: boolean = false;
+  @Input() position?: number = null;
+
+  @Output() hasChanges = new EventEmitter<boolean>();
+  @Output() handleBitSets = new EventEmitter<Field>();
+  @Output() handleBitSetsOfComposite = new EventEmitter<HandleBitSet>();
+
+  formControl!: FormControl;
+  form!: FormGroup;
+  hideField: boolean = null;
+
+  constructor(private rootFormGroup: FormGroupDirective, private formControlService: FormControlService) {
+  }
+
+  ngOnInit() {
+    if (this.position !== null) {
+      this.form = this.rootFormGroup.control.controls[this.position] as FormGroup;
+    } else {
+      this.form = this.rootFormGroup.control;
+    }
+    this.formControl = this.form.get(this.fieldData.name) as FormControl;
+
+    if(this.fieldData.form.dependsOn) {
+      // console.log(this.fieldData.form.dependsOn);
+      this.enableDisableField(this.form.get(this.fieldData.form.dependsOn.name).value, this.fieldData.form.dependsOn.value);
+
+      this.form.get(this.fieldData.form.dependsOn.name).valueChanges.subscribe(
+        value => {
+            this.enableDisableField(value, this.fieldData.form.dependsOn.value);
+          },
+        error => {console.log(error)}
+      );
+    }
+
+    // console.log(this.fieldData);
+    // console.log(this.form);
+    // console.log(this.formControl);
+  }
+
+  /** Handle Arrays --> **/
+
+  fieldAsFormArray() {
+    return this.formControl as unknown as FormArray;
+  }
+
+  push() {
+    this.fieldAsFormArray().push(this.formControlService.createField(this.fieldData));
+  }
+
+  remove(field: string, i: number) {
+    this.fieldAsFormArray().removeAt(i);
+  }
+
+  /** check fields validity--> **/
+
+  checkFormValidity(): boolean {
+    return (!this.formControl.valid && (this.formControl.touched || this.formControl.dirty));
+  }
+
+  checkFormArrayValidity(position: number): boolean {
+    return ((this.fieldAsFormArray().get([position]).invalid && this.fieldAsFormArray().get([position]).touched)
+      && (this.editMode || this.fieldAsFormArray().get([position]).dirty));
+  }
+
+  /** Bitsets--> **/
+
+  updateBitSet(fieldData: Field) {
+    this.timeOut(200).then(() => { // Needed for radio buttons strange behaviour
+      if (fieldData.form.mandatory) {
+        this.handleBitSets.emit(fieldData);
+      }
+    });
+  }
+
+  /** other stuff--> **/
+  unsavedChangesPrompt() {
+    // console.log('string-field.component emitting to parent->');
+    this.hasChanges.emit(true);
+  }
+
+  enableDisableField(value, enableValue) {
+    if (value?.toString() == enableValue) {
+      this.formControl.enable();
+      this.hideField = false;
+    } else {
+      this.formControl.disable();
+      this.formControl.reset();
+      this.hideField = true;
+      // maybe add this if the remaining empty fields are a problem
+      // (this.formControl as unknown as FormArray).clear();
+    }
+  }
+
+  timeOut(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+}
