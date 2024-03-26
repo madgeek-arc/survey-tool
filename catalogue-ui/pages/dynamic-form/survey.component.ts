c6ec731f86103ec3dcdc48081df9f1601eafe934
jbalasis <cs2180008@di.uoa.gr>
2024-03-26 18:49:51 +0200

    fix bug on getControlValue, lowered ckeditor version to remove powered by link

--- catalogue-ui/pages/dynamic-form/survey.component.ts
+++ catalogue-ui/pages/dynamic-form/survey.component.ts
@@ -1,42 +1,22 @@
-import {
-  Component,
-  DestroyRef,
-  EventEmitter,
-  inject,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  SimpleChanges
-} from "@angular/core";
-import { UntypedFormArray, UntypedFormBuilder, UntypedFormGroup } from "@angular/forms";
+import { Component, DestroyRef, EventEmitter, inject, Input, OnChanges, OnInit, Output, SimpleChanges } from "@angular/core";
+import { AbstractControl, FormArray, FormGroup, UntypedFormArray, UntypedFormBuilder, UntypedFormGroup } from "@angular/forms";
 import { Router } from "@angular/router";
 import { FormControlService } from "../../services/form-control.service";
-import { Section, Field, Model, Tabs } from "../../domain/dynamic-form-model";
-import {
-  Columns,
-  Content,
-  DocDefinition,
-  PdfImage,
-  PdfMetadata,
-  PdfTable,
-  TableDefinition
-} from "../../domain/PDFclasses";
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-import BitSet from "bitset";
+import { PdfGenerateService } from "../../services/pdf-generate.service";
+import { Section, Field, Model, Tabs } from "../../domain/dynamic-form-model"
 
-import UIkit from "uikit";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { debounceTime, distinctUntilChanged } from "rxjs/operators";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import UIkit from "uikit";
+import BitSet from "bitset";
+
 declare var require: any;
 const seedRandom = require('seedrandom');
 
 @Component({
   selector: 'app-survey',
   templateUrl: 'survey.component.html',
-  providers: [FormControlService]
+  providers: [FormControlService, PdfGenerateService]
 })
 
 export class SurveyComponent implements OnInit, OnChanges {
@@ -74,7 +54,8 @@ export class SurveyComponent implements OnInit, OnChanges {
   previousValue: any = {};
   changedField: string | null = null;
 
-  constructor(private formControlService: FormControlService, private fb: UntypedFormBuilder, private router: Router) {
+  constructor(private formControlService: FormControlService, private pdfService: PdfGenerateService,
+              private fb: UntypedFormBuilder, private router: Router) {
     this.form = this.fb.group({});
   }
 
@@ -141,9 +122,10 @@ export class SurveyComponent implements OnInit, OnChanges {
       this.form.valueChanges.pipe(
         takeUntilDestroyed(this.destroyRef),
         debounceTime(1000),
-        distinctUntilChanged()).subscribe(changes => {
+        distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))).subscribe(changes => {
         this.changedField = this.detectChanges(changes, this.previousValue, '');
         console.log(this.changedField);
+        console.log(this.getControlValue(this.changedField));
         this.previousValue = { ...changes };
       });
 
@@ -160,6 +142,7 @@ export class SurveyComponent implements OnInit, OnChanges {
     }
   }
 
+  /** Find changed field and get value --> **/
   detectChanges(currentValue: any, previousValue: any, path: string): string | null {
     for (const field of Object.keys(currentValue)) {
       const currentPath = path ? `${path}.${field}` : field;
@@ -183,7 +166,7 @@ export class SurveyComponent implements OnInit, OnChanges {
 
   detectArrayChanges(currentArray: any[], previousArray: any[], path: string): string | null {
     for (let i = 0; i < Math.max(currentArray.length, previousArray.length); i++) {
-      const currentPath = `${path}[${i}]`;
+      const currentPath = `${path}.[${i}]`;
 
       if (currentArray[i] instanceof Object && previousArray[i] instanceof Object) {
         const nestedChanges = this.detectChanges(currentArray[i], previousArray[i], currentPath);
@@ -196,6 +179,23 @@ export class SurveyComponent implements OnInit, OnChanges {
     }
     return null;
   }
+
+  getControlValue(path: string): any {
+    const segments = path.split('.');
+    let control: AbstractControl | null = this.form;
+    for (const segment of segments) {
+      if (control instanceof FormGroup) {
+        control = control.get(segment);
+      } else if (control instanceof FormArray) {
+        const index = Number(segment.split('[')[1].split(']')[0]);
+        control = control.at(index);
+      } else {
+        break;
+      }
+    }
+    return control ? control.value : null;
+  }
+  /** <-- Find changed field and get value **/
 
   validateForm() {
     for (const chapterChangeMapElement of this.chapterChangeMap) {
@@ -386,221 +386,8 @@ export class SurveyComponent implements OnInit, OnChanges {
 
   /** Generate PDF --> **/
   generatePDF() {
-    let docDefinition: DocDefinition = new DocDefinition();
-    docDefinition.content.push(new Content(this.model.name, ['title']));
-    if (this.model.notice)
-      docDefinition.content.push({text: this.strip(this.model.notice), italics: true, alignment: 'justify'});
-    docDefinition.info = new PdfMetadata(this.model.name);
-
-    let description = 'none';
-    if (this.model.name === 'Survey on National Contributions to EOSC 2022' || this.model.id === 'm-eosc-sb-2023') {
-      description = 'end'
-    }
-    this.createDocumentDefinition(this.form, docDefinition, description);
-
-    pdfMake.createPdf(docDefinition).download(this.model.name);
+    this.pdfService.generatePDF(this.model, this.payload);
   }
-
-  documentDefinitionRecursion(fields: Field[], docDefinition: DocDefinition, description: string, descriptionAtEnd?: DocDefinition) {
-    for (const field of fields) {
-      if (field.deprecated)
-        continue;
-      if (field.label.text)
-        docDefinition.content.push(new Content(field.label.text, ['mx_3']));
-      if (field.form.description.text
-        && !field.form.description.text.includes('Please add only new use cases as any use cases submitted in the previous survey will be imported here')
-        && !field.form.description.text.includes('For example on curricula for data stewardship')
-      ) {
-        if (description === 'end') {
-          let questionNumber = null;
-          if (field.label.text) {
-            questionNumber = field.label.text.split('. ')[0];
-          }
-          // let term = field.form.description.text.split('-')[0]
-          // console.log(this.strip(field.form.description.text));
-
-          if (this.model.id === 'm-eosc-sb-2023') {
-            let strArray = [];
-            if (this.strip(field.form.description.text).includes('Instructions')) {
-              if (this.strip(field.form.description.text).includes('Glossary'))
-                strArray[0] = this.string_between_strings('Instructions', 'Glossary', this.strip(field.form.description.text));
-              else
-                strArray[0] = this.strip(field.form.description.text).split('Instructions')[1];
-              // console.log(this.strip(field.form.description.text));
-            }
-            if (field.form.description.text.split('Glossary').length > 1)
-              strArray[1] = field.form.description.text.split('Glossary')[1];
-
-            let content = {
-              style: ['mt_3'],
-              text: []
-            }
-            content.text.push({text: questionNumber+' '});
-            let terms = strArray[1]?.split('<br><br>')
-            if (terms?.length > 0) {
-              for (let i = 0; i < terms.length; i++) {
-                content.text.push({text:  this.strip(terms[i].split(':')[0])+': ', bold: true});
-                content.text.push({text:  this.strip(terms[i].split(':')[1]).concat('\n')});
-              }
-            }
-            content.text.push({text:  strArray[0] ? 'Instructions' : '', bold: true});
-            content.text.push({text: strArray[0] ? ' - '+strArray[0] : ''});
-
-
-            // descriptionAtEnd.content.push(new Content(questionNumber + ' ' + components.shift() + '-' + components.join('-'), ['mt_3']));
-            if (strArray.length)
-              descriptionAtEnd.content.push(content);
-
-          } else {
-            let components = this.strip(field.form.description.text).split(' - ');
-            let content = {
-              style: ['mt_3'],
-              text: [
-                questionNumber,
-                ' ',
-                {text:  components.shift(), bold: true},
-                ' - ',
-                components.join('-')
-              ]
-            }
-            // descriptionAtEnd.content.push(new Content(questionNumber + ' ' + components.shift() + '-' + components.join('-'), ['mt_3']));
-            descriptionAtEnd.content.push(content);
-          }
-
-        }
-        if (description === 'show')
-          docDefinition.content.push(new Content(field.form.description.text, ['mt_3']));
-      }
-      let answerValues = this.findVal(this.payload?.answer, field.name);
-      if (field.typeInfo.type === 'radio') {
-        let values = field.typeInfo.values
-        // if (field.kind === 'conceal-reveal')
-        //   values = this.getModelData(this.model.sections, field.parent).typeInfo.values;
-        for (const value of values) {
-          let content = new Columns();
-          if (value === answerValues?.[0]){
-            content.columns.push(new PdfImage('radioChecked', 10, 10, ['marginTopCheckBox']));
-          }
-          else {
-            content.columns.push(new PdfImage('radioUnchecked', 10, 10, ['marginTopCheckBox']));
-          }
-          content.columns.push(new Content(value,['ms_1', 'mt_1']));
-          docDefinition.content.push(content);
-        }
-      } else if (field.typeInfo.type === 'checkbox') {
-        docDefinition.content.pop();
-        let content = new Columns(['mx_1']);
-        if (answerValues?.[0]) {
-          content.columns.push(new PdfImage('checked', 10, 10, ['mt_1']));
-        } else {
-          content.columns.push(new PdfImage('unchecked', 10, 10, ['mt_1']));
-        }
-        content.columns.push(new Content(field.label.text,['ms_1']));
-        docDefinition.content.push(content);
-      } else if (field.typeInfo.type === 'largeText' || field.typeInfo.type === 'richText') {
-        if (answerValues?.[0]) {
-          docDefinition.content.push(new PdfTable(new TableDefinition([[this.strip(answerValues[0])]], ['*']), ['mt_1']));
-        } else {
-          docDefinition.content.push(new PdfTable(new TableDefinition([['']],['*'], [48]), ['mt_1']));
-        }
-      } else if (field.typeInfo.type !== 'composite') {
-        if (answerValues?.[0]) {
-          docDefinition.content.push(new PdfTable(new TableDefinition([[answerValues[0]]], ['*']), ['mt_1']));
-        } else if (field.form.placeholder) {
-          docDefinition.content.push(new PdfTable(new TableDefinition([[{text: field.form.placeholder, color: 'gray'}]],['*']), ['mt_1']));
-        } else {
-          docDefinition.content.push(new PdfTable(new TableDefinition([['']],['*'], [16]), ['mt_1']));
-        }
-      } else if (field.kind === 'external' && field.typeInfo.type === 'composite') {
-        docDefinition.content.push(new PdfTable(new TableDefinition([[{text: field.form.description, color: 'gray'}]],['*']), ['mt_1']));
-        continue;
-      }
-      if (field.subFields)
-        this.documentDefinitionRecursion(field.subFields, docDefinition, description, descriptionAtEnd);
-    }
-  }
-
-  createDocumentDefinition(group: UntypedFormGroup | UntypedFormArray, docDefinition: DocDefinition, description: string) {
-    let descriptionsAtEnd = new DocDefinition();
-
-    if (this.model.name === 'Survey on National Contributions to EOSC 2022'  || this.model.id === 'm-eosc-sb-2023') {
-      docDefinition.content.push(new Content('Definitions of key terms can be found in Appendix A', ['mt_3']));
-    }
-
-    this.model.sections.sort((a, b) => a.order - b.order);
-    for (const section of this.model.sections) {
-      section.subSections.sort((a, b) => a.order - b.order);
-      if (this.model.sections.length > 1) {
-        docDefinition.content.push(new Content(section.name, ['chapterHeader']));
-      }
-
-      for (const subSection of section.subSections) {
-        if (section.subSections.length > 1) {
-          docDefinition.content.push(new Content(subSection.name, ['tabHeader']));
-        }
-        if (subSection.fields)
-          this.documentDefinitionRecursion(subSection.fields, docDefinition, description, descriptionsAtEnd);
-      }
-
-    }
-
-    if (this.model.name === 'Survey on National Contributions to EOSC 2022' || this.model.id === 'm-eosc-sb-2023') {
-
-      docDefinition.content.push(new Content('Appendix A', ['title']));
-      docDefinition.content.push(...descriptionsAtEnd.content);
-
-      docDefinition.content.push(new Content('Appendix B', ['title']));
-      let content = [
-        {
-          style: ['mt_3'],
-          text: ['Visit the ',
-            {text: 'EOSC Observatory', link: 'https://eoscobservatory.eosc-portal.eu', color: 'cornflowerblue', decoration: 'underline'},
-            ' to explore the data from the EOSC Steering Board surveys on National Contributions to EOSC and visit the ',
-            {text: 'EOSC Observatory Zenodo Community', link: 'https://zenodo.org/communities/eoscobservatory', color: 'cornflowerblue', decoration: 'underline'},
-            ' to access all key outputs related to the surveys and EOSC Observatory.']
-        }
-      ]
-      docDefinition.content.push(content);
-    }
-
-    return;
-
-  }
-
-  findVal(obj, key) {
-    if (!obj)
-      return null;
-    let seen = new Set, active = [obj];
-    while (active.length) {
-      let new_active = [], found = [];
-      for (let i=0; i<active.length; i++) {
-        Object.keys(active[i]).forEach(function(k){
-          let x = active[i][k];
-          if (k === key) {
-            found.push(x);
-          } else if (x && typeof x === "object" &&
-            !seen.has(x)) {
-            seen.add(x);
-            new_active.push(x);
-          }
-        });
-      }
-      if (found.length) return found;
-      active = new_active;
-    }
-    return null;
-  }
-
-  strip(html){
-    let doc = new DOMParser().parseFromString(html, 'text/html');
-    return doc.body.textContent || "";
-  }
-
-  string_between_strings(startStr: string, endStr: any, str: string) {
-    let pos = str.indexOf(startStr) + startStr.length;
-    return str.substring(pos, str.indexOf(endStr, pos));
-  }
-
   /** <-- Generate PDF **/
 
   /** other stuff --> **/
