ba6e5cc4cdfe1200313e915a217471ee8081d82f
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2025-07-01 00:01:50 +0300

    chore: preparing the survey tool sidebar to have a footer and also added the menu icons in the sidebar mobile toggle component

--- app/shared/dashboard-side-menu/mobile-toggle/sidebar-mobile-toggle.component.ts
+++ app/shared/dashboard-side-menu/mobile-toggle/sidebar-mobile-toggle.component.ts
@@ -2,22 +2,23 @@ import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardSideMenuService, SidebarItem } from "../dashboard-side-menu.service";
 import { Subscription } from "rxjs";
+import { IconsComponent } from "../../../utils/icons/icons.component";
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconsComponent],
   selector: 'sidebar-mobile-toggle',
   template: `
     <a *ngIf="activeSidebarItem" href="#sidebar_offcanvas" class="sidebar_mobile_toggle uk-link-reset uk-width-3-5 uk-flex uk-flex-middle" uk-toggle>
-<!--      <div *ngIf="activeSidebarItem.icon && (activeSidebarItem.icon.svg || activeSidebarItem.icon.name)" class="uk-width-auto">-->
-<!--        <icon class="menu-icon" [customClass]="activeSidebarItem.icon.class" [name]="activeSidebarItem.icon.name" ratio="0.8" [svg]="activeSidebarItem.icon.svg" [flex]="true"></icon>-->
-<!--      </div>-->
+      <div *ngIf="activeSidebarItem.icon && (activeSidebarItem.icon.svg || activeSidebarItem.icon.name)" class="uk-width-auto">
+        <icon class="menu-icon" [customClass]="activeSidebarItem.icon.class" [name]="activeSidebarItem.icon.name" [ratio]="activeSidebarItem.icon.ratio?activeSidebarItem.icon.ratio:0.8" [svg]="activeSidebarItem.icon.svg" [flex]="true"></icon>
+      </div>
       <span class="uk-width-expand uk-text-truncate uk-margin-small-left uk-text-bolder">
           {{activeSidebarItem.name}}
         <span *ngIf="activeSidebarItem.subItem">- {{activeSidebarItem.subItem.name}}</span>
       </span>
       <div class="uk-width-auto uk-margin-small-left">
-<!--        <icon name="arrow_right" ratio="1.4"  [flex]="true"></icon>-->
+        <icon name="arrow_right" [ratio]=1.4></icon>
       </div>
     </a>
   `
