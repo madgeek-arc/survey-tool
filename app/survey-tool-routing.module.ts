6272d9878786cdeacb8288d4bb8666d3467bb777
Yiannis Balasis <j.balasis92@gmail.com>
2026-02-26 19:05:12 +0200

    refactor: Split form builder page to two components and two corresponding paths.

--- app/survey-tool-routing.module.ts
+++ app/survey-tool-routing.module.ts
@@ -4,11 +4,22 @@ import { FormBuilderComponent } from "../catalogue-ui/pages/form-builder/form-bu
 import { AcceptInvitationComponent } from "./pages/accept-invitation.component.ts/accept-invitation.component";
 import { AuthGuard } from "./services/auth-guard.service";
 import { UserProfileComponent } from "./pages/user-profile/user-profile.component";
+import { FormsListComponent } from "../catalogue-ui/pages/forms-list/forms-list.component";
 
 const observatoryUiRoutes: Routes = [
 
   {
     path: 'fb',
+    component: FormsListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'fb/new-form',
+    component: FormBuilderComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'fb/:id/edit',
     component: FormBuilderComponent,
     canActivate: [AuthGuard]
   },
